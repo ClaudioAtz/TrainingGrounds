14c9752ac984b617a5ca22978ef2282b750889d1
Claudio <claudio.atz@gmail.com>
2017-06-30 11:24:03 +0100

    Refactoring. Lots of.

--- Source/TrainingGrounds/TrainingGroundsGameMode.cpp
+++ Source/TrainingGrounds/TrainingGroundsGameMode.cpp
@@ -3,13 +3,13 @@
 #include "TrainingGrounds.h"
 #include "TrainingGroundsGameMode.h"
 #include "TrainingGroundsHUD.h"
-#include "TrainingGroundsCharacter.h"
+#include "Player/FirstPersonCharacter.h"
 
 ATrainingGroundsGameMode::ATrainingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
