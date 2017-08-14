c5c06837e24a800d19cc2347d9cc5a92de054f46
Claudio <claudio.atz@gmail.com>
2017-08-14 17:28:18 +0100

    Fixed issue where projectiles were still hitting dead enemies.

--- Source/TrainingGrounds/Weapons/BallProjectile.cpp
+++ Source/TrainingGrounds/Weapons/BallProjectile.cpp
@@ -10,7 +10,7 @@ ABallProjectile::ABallProjectile()
 	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
 	CollisionComp->InitSphereRadius(5.0f);
 	CollisionComp->BodyInstance.SetCollisionProfileName("Projectile");
-	CollisionComp->OnComponentHit.AddDynamic(this, &ABallProjectile::OnHit);		// set up a notification for when this component hits something blocking
+	//CollisionComp->OnComponentHit.AddDynamic(this, &ABallProjectile::OnHit);		// set up a notification for when this component hits something blocking
 
 	// Players can't walk on it
 	CollisionComp->SetWalkableSlopeOverride(FWalkableSlopeOverride(WalkableSlope_Unwalkable, 0.f));
@@ -29,17 +29,6 @@ ABallProjectile::ABallProjectile()
 
 	// Die after 3 seconds by default
 	InitialLifeSpan = 3.0f;
-}
-
-void ABallProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
-{
-	// Only add impulse and destroy projectile if we hit a physics
-	if ((OtherActor != NULL) && (OtherActor != this) && (OtherComp != NULL) && OtherComp->IsSimulatingPhysics())
-	{
-		OtherComp->AddImpulseAtLocation(GetVelocity() * 100.0f, GetActorLocation());
-
-		Destroy();
-	}
 }
 
 bool ABallProjectile::IsFriendlyFire() const
